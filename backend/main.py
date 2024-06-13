b60bc8125224299d8aadcb74bdf5365f28083780
acharyabibash <acharabibash@outlook.com>
2024-06-13 15:47:26 +0545

    Migrations

--- backend/main.py
+++ backend/main.py
@@ -16,18 +16,18 @@ from wtforms import Form,StringField,FloatField,FileField
 from werkzeug.utils import secure_filename
 import os
 import logging
-from config import db
+# from config import db
 app = Flask(__name__)
 # from flask_sqlalchemy import SQLAlchemy
 # from flask import Flask
 # from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # Change as needed
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 # Association table for many-to-many relationship between TravelLog and TravelLogDocument
 travel_log_document_association = db.Table(
     'travel_log_document_association',
