6972008db3be7e66d087714829d6fed694cee760
rabinam24 <rabinbhandari070@gmail.com>
2024-07-03 16:07:21 +0545

    docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir:'buildStaticReactVite' //default build directory for react-vite is dist but this overwrites build directoty as buildStaticReactVite
+//   }
+// })
+
+export default {
   build: {
-    outDir:'buildStaticReactVite' //default build directory for react-vite is dist but this overwrites build directoty as buildStaticReactVite
+    outDir: 'dist'
   }
-})
+}
