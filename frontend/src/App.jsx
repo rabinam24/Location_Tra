c08ee1749dca957e06fdb61504a8ff124a9dd4dc
rabinam24 <rabinbhandari070@gmail.com>
2024-07-31 21:43:12 +0545

    hoho

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,27 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NewLanding from "./components/NewLanding";
 import Profile from "./Routes/Profile";
 import Home from "./Routes/Homepage";
-import { requestPermission, onMessageListener } from "./firebase"; // Import the functions
-import { register } from "./serviceWorkerRegistration"; // Import the service worker registration
+
 
 function Apps() {
-  useEffect(() => {
-    register();
-  }, []);
-
-  useEffect(() => {
-    requestPermission();
-
-    onMessageListener()
-      .then((payload) => {
-        console.log("Message received. ", payload);
-        // Customize notification handling here if needed
-      })
-      .catch((err) => {
-        console.log("Failed to receive message. ", err);
-      });
-  }, []);
-
   return (
     <Router>
       <Routes>
